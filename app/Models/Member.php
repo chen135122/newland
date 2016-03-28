d4edb08288dd631b4596819c28be8b24c53cecae
zhaihx <zhaihx@chitunet.com>
2016-03-28 12:45:24 +0800

    1

---
+++ app/Models/Member.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by chitunet.com.
+ * User: yin
+ * Date: 2016/3/1
+ * Time: 15:57
+ */
+namespace App\Models;
+
+class Member extends BaseModel
+{
+    protected $table = 'nz_member';
+}
