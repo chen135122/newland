1f0958daaa7bc97fc6dff7fb2ddd5432eaf86784
Alex <dinghua@me.com>
2016-03-29 11:22:15 +0800

    debug 5

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -73,7 +73,7 @@ Route::group([ 'middleware' => ['web']], function () {
     Route::get('query','\App\Http\Controllers\AlipayController@query');
 });
 Route::get('debug', function(){
-  return 3;
+  return 5;
 });
 
 
