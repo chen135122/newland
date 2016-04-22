61d8c24b72e3c2a09d102a8284ace385ec2a2b19
alaric888 <1227037022@qq.com>
2016-04-22 09:47:13 +0800

    添加统计代码

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -50,6 +50,16 @@
     </style>
 
     @stack('style')
+    <script>
+        var _hmt = _hmt || [];
+        (function() {
+            var hm = document.createElement("script");
+            hm.src = "//hm.baidu.com/hm.js?f61c8954a13efd3b3bc39172386a46cf";
+            var s = document.getElementsByTagName("script")[0];
+            s.parentNode.insertBefore(hm, s);
+        })();
+    </script>
+
 </head>
 <body>
 
