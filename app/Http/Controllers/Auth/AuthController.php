5181c2e16efe535c6f28214871e19ca1969fd2de
zhaihx <zhaihx@chitunet.com>
2016-06-05 21:24:59 +0800

    only

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -115,12 +115,15 @@ class AuthController extends Controller
                 $mobile=$userarry->openid;
                 $password=$userarry->openid;
                 $paramcount = User::where('mobile', '=',$mobile)->count();
+                if($paramcount<=0)
+                {
                 $user->password= $mobile;
                 $user->status=1;
                 $user->mobile=$mobile;
                 $user->nickname=$userarry->nickname;
                 $user->address=$userarry->country.",".$userarry->province.",".$userarry->city;
                 $user->save();
+                }
             }
         }else{
             return view('auth.login');
