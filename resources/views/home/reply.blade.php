25fa700e4e08fc7d895eb236e8062e7243c749dc
zhaihx <zhaihx@chitunet.com>
2016-06-07 11:57:30 +0800

    reply

--- resources/views/home/reply.blade.php
+++ resources/views/home/reply.blade.php
@@ -1,7 +1,6 @@
 <?php
 require_once app_path().'/WxPayPubHelper/customer.php';
 if($_GET['action'] == "reply"){
-    echo "1";
     $touser = $_POST['tousername'];
     $content = $_POST['content'];
     $token=$_POST["token"];
