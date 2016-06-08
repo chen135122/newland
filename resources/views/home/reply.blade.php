c36c63bbef19a396f9f51772532eef0b7db1fb13
zhaihx <zhaihx@chitunet.com>
2016-06-08 12:47:34 +0800

    2

--- resources/views/home/reply.blade.php
+++ resources/views/home/reply.blade.php
@@ -47,13 +47,6 @@ $result=$tem->doSend(urldecode(json_encode($template)));
 <script>
     if("<?php echo $result->errcode; ?>" =="0")
     {
-        var userAgent = navigator.userAgent;
-        if (userAgent.indexOf("Firefox") != -1 || userAgent.indexOf("Chrome") !=-1) {
-            window.location.href="about:blank";
-        } else {
-            window.opener = null;
-            window.open("", "_self");
-            window.close();
-        }
+        window.location="http://m.allinnewzealand.com/app/site/index";
     }
 </script>
