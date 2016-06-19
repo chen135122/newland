2f437f2c6651dac021f7c4f95487eadd2c2fc6de
zhaihx <zhaihx@chitunet.com>
2016-06-19 21:11:56 +0800

    1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -80,10 +80,6 @@ class HomeController extends Controller
     }
     public  function login(Request $request)
     {
-<<<<<<< HEAD
-=======
-//        return redirect()->guest("http://m.allinnewzealand.com/auth/login?txtMobile=onjnuslHgho_parKy-dJy-1kUlfc&password=123456&uuid=2");
->>>>>>> 7639e34f1c966cea88944b06c5934a8f9feafbc9
         $user=new User();
         $userid="";
         $newmobile="";
@@ -130,7 +126,6 @@ class HomeController extends Controller
                 $uuid=$request->get("uuid");
                 if(isset($uuid))
                 {
-<<<<<<< HEAD
                         if(strval($uuid)=="2")
                         {
                            return redirect()->guest("http://m.allinnewzealand.com/auth/login?txtMobile=".$mobile."&password=123456&uuid=2");
@@ -149,32 +144,11 @@ class HomeController extends Controller
                             $usersta->status=1;
                             $usersta->save();
                        }
-=======
-//                    return redirect()->guest("http://m.allinnewzealand.com/auth/login?txtMobile=".$mobile."&password=123456&uuid=2");
-//                  return redirect()->guest("http://m.allinnewzealand.com/auth/login?txtMobile=".$mobile."&password=123456");
-                    if(strval($uuid)=="2")
-                    {
-                        return redirect()->guest("http://m.allinnewzealand.com/auth/login?txtMobile=".$mobile."&password=123456&uuid=2");
-                    }
-                    elseif(strval($uuid)=="3")
-                    {
-                        return redirect()->guest("http://m.allinnewzealand.com/auth/login?txtMobile=".$mobile."&password=123456&uuid=3");
-                    }
-                    elseif(strval($uuid)=="1")
-                        return redirect()->guest("http://m.allinnewzealand.com/auth/login?txtMobile=".$mobile."&password=123456");
-                    else
-                        $usersta=new UserStatus();
-                        $usersta->uuid=$uuid;
-                        $usersta->cusid= $userid;
-                        $usersta->status=1;
-                        $usersta->save();
->>>>>>> 7639e34f1c966cea88944b06c5934a8f9feafbc9
                 }
             }
         }else{
             //return view('auth.login');
         }
-        //return redirect()->guest("/auth/login?txtMobile=".$newmobile."&password=".$newpassword);
         return view("home.login")->with("oppenid",$oppenid)->with("nickname",$user->nickname);//->with("uuid",$uuid)->with("userid",$userid)->;
     }
 
