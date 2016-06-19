1721a1ef7ede55e909472bdfc625287aa685f6c1
alaric888 <1227037022@qq.com>
2016-06-19 12:03:27 +0800

    修改链接

--- resources/views/trust/show.blade.php
+++ resources/views/trust/show.blade.php
@@ -26,7 +26,7 @@
     <div class="container">
         <ul>
             <li><a href="/">首页</a></li>
-            <li><a href="/partner">家庭信托</a></li>
+            <li><a href="/trust">家庭信托</a></li>
         </ul>
     </div>
 </div>
