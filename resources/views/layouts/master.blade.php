4cb48c6678aff6ac5e45a80346eef4c67e9a0b57
alaric888 <1227037022@qq.com>
2016-06-24 15:25:31 +0800

    修改

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,7 +8,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="author" content="Ansonika">
-    <title>@yield('title','几维说- 梦想生活从这里起航')</title>
+    <title>@yield('title','几维说- 梦想生活从这里启航')</title>
     <link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon">
     <!-- BASE CSS -->
     <link href="/css/base.css" rel="stylesheet">
@@ -153,28 +153,7 @@
     </header>
 
 @yield('content')
-
-<footer>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-2 col-sm-12 col-xs-12 ">
-                <img src="/img/logo_sticky2.png" height="60" alt="All IN New Zealand" data-retina="true" class="logo_sticky">
-            </div>
-            <div class="col-md-8  col-sm-6 col-xs-12" >
-                <p class="footer_l">
-                    南京布维金广投资管理有限公司 版权所有&nbsp;&nbsp;<a href="http://www.miitbeian.gov.cn" target="_blank">苏ICP备16007960号</a> <br/>
-                  客服热线：+86-25-58761818转0&nbsp;&nbsp;&nbsp;邮箱：services@allinnewzealand.com<br/>
-                     地址：江苏省南京市鼓楼区广州路189号民防大厦801&nbsp;&nbsp;&nbsp;
-                    产品体验店：江苏省南京市北京东路8号江苏省广播电视台2F
-                </p>
-            </div>
-            <div class="col-md-2 col-sm-6 col-xs-12" >
-                <img src="/img/showqrcode.jpg" width="100" height="100"/>
-            </div>
-        </div>
-
-    </div>
-</footer>
+@include('layouts.footer')
 <div id="toTop"></div>
 <div id="rightArrow"><a href="javascript:;" title="在线客户"></a></div>
 <div id="floatDivBoxs">
