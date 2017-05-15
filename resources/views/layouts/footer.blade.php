b7414dea8333ae83e8fbbc99e03c9eb805987d1e
Your Name <you@example.com>
2017-05-15 11:22:54 +0800

    change phone number

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -7,9 +7,8 @@
             <div class="col-md-8  col-sm-6 col-xs-12" >
                 <p class="footer_l">
                     南京布维金广投资管理有限公司 版权所有&nbsp;&nbsp;<a href="http://www.miitbeian.gov.cn" target="_blank">苏ICP备16007960号</a> <br/>
-                    客服热线：+86-25-58761818转0&nbsp;&nbsp;&nbsp;邮箱：services@allinnewzealand.com<br/>
+                    客服热线：400-828-3365 &nbsp;&nbsp;&nbsp;邮箱：services@allinnewzealand.com<br/>
                     地址：江苏省南京市鼓楼区广州路189号民防大厦801&nbsp;&nbsp;&nbsp;
-                    产品体验店：江苏省南京市北京东路8号江苏省广播电视台2F
                 </p>
             </div>
             <div class="col-md-2 col-sm-6 col-xs-12" >
--- resources/views/layouts/side_contact.blade.php
+++ resources/views/layouts/side_contact.blade.php
@@ -1,6 +1,6 @@
 <div class="box_style_4">
     <i class="icon_set_1_icon-57"></i>
     <h4>需要 <span>帮助?</span></h4>
-    <a href="tel:86-25-58761818" class="phone">+86-25-58761818<span style="color:#85c99d;">转0</span></a>
+    <a href="tel:4008283365" class="phone">400-828-3365</a>
     <small>周一 至 周日  8:30 - 18:00</small>
 </div>
